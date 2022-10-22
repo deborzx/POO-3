31bb32e835d36bd8929fe6616f5118b2c935e00c
Debora Oliveira <debora.ramos.2@outlook.com>
2022-10-22 13:46:53 -0300

    Adicionar arquivos de projeto.

---
+++ POO-3/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //formas geométricas: line, quad, rect, tri, circ
+
+            //instanciando pela class quad
+            IDrawable shape = new Quad();
+            shape.Draw();
+
+        Console.ReadLine();
+
+        }
+    }
+    
+    //criando interface 
+    interface IDrawable
+    {
+        //metodo
+        void Draw();
+        float Area();
+
+    }
+
+
+
+    //criando classes para as formas 
+    class Line : IDrawable
+    {
+        public float Area()
+        {
+            return 20;
+        }
+
+        public void Draw()
+        {
+            Console.WriteLine("-----linha-----");
+        }
+    }
+
+    class Quad : IDrawable
+    {
+        public float Area()
+        {
+            return 2;
+        }
+
+        public void Draw()
+        {
+            Console.WriteLine("-----quadrado-----");
+        }
+    }
+    class Rect : IDrawable
+    {
+        public float Area()
+        {
+            return 10;
+        }
+
+        public void Draw()
+        {
+            Console.WriteLine("-----retangulo-----");
+        }
+    }
+
+}
